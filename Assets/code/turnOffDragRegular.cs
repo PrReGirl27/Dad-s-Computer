26e20ad8bb73355e952ecf6eb30ab66aab0eda20
PrReGirl27 <priarjohnson@gmail.com>
2025-01-09 19:30:36 -0500

    Update and rename idk.cs to turnOffDragRegular.cs

--- Assets/code/turnOffDragRegular.cs
+++ Assets/code/turnOffDragRegular.cs
@@ -11,19 +11,20 @@ using UnityEngine.UI;
 
     public class idk : MonoBehaviour, IPointerDownHandler,  IPointerUpHandler, IDragHandler
 
-    {
+     {
 
 
     public void OnPointerDown(PointerEventData eventData)
         {
        drag.canDrag = false;
-           
+          // if a button is being clicked, the panel can't be dragged 
             Debug.Log("clciked");
         }
     
     public void OnPointerUp(PointerEventData eventData)
 
     {
+    // if a button is not being clicked, the panel can be dragged
         drag.canDrag = true;
         Debug.Log("notclciked");
     }
@@ -34,6 +35,7 @@ using UnityEngine.UI;
 
     public void OnDrag(PointerEventData eventData)
     {
+    // if a button is being dragged, the panel can't be dragged
         drag.canDrag = false;
 
         Debug.Log("clciked");
